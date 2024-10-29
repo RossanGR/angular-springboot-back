2af2bedbd62992c60269b064d074c57a6f47a314
Rossan <gr.rossan@gmail.com>
2024-10-29 17:35:15 -0300

    Feat: Implementacao do CRUD

--- src/main/java/com/angularSpringBoot/demo/controller/CoursesController.java
+++ src/main/java/com/angularSpringBoot/demo/controller/CoursesController.java
@@ -3,9 +3,9 @@ import com.angularSpringBoot.demo.model.Course;
 import com.angularSpringBoot.demo.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +23,48 @@ public class CoursesController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    // Primeira forma de fazer o cadastro no banco de um dado e fazer o retorno para o front-end.
+    // Response Entity é essecial para mostrar o status da requisição(se foi criado, ou deu erro)
+    // @PostMapping
+    // public ResponseEntity<Course> create (@RequestBody Course course){
+    //    return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    //}
+    // Segunda forma de fazer o cadastro e o retorno para o front
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course){
+        return courseRepository.findById(id)
+                .map(record -> {
+                    record.setName(course.getName());
+                    record.setCategory(course.getCategory());
+//                    Course update = ;
+                    return ResponseEntity.ok().body(courseRepository.save(record));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return courseRepository.findById(id)
+                .map(record -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
